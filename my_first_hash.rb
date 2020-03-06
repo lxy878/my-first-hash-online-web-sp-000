704b56c86d2b61770546e5dd3f545fe3249631e4
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 13:01:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  return {"whale bone corsets"=>2, "oil paintings"=>5, "procelain vases"=>3}
+  return {"whale bone corsets"=>2, "oil paintings"=>5, "porcelain vases"=>3}
 
 end
 
